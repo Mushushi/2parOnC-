a5d97602e07838e07ffc8fa9360a5a86989ddc9e
Mushishi <32162981+Mushushi@users.noreply.github.com>
2021-02-08 16:00:02 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main ( int a, char colon, int b ){
+    cout << "Input two parameters using , : ";
+    cin >> a >>colon >> b;
+    cout << a << ", " << b;
+} 
